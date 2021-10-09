24fd52078f7d596c27130db4edaaecbc0465c981
JEREMY MADRIAGA <2200093@slu.edu.ph>
2021-10-09 23:35:00 +0800

    -car cases

--- carcases.js
+++ carcases.js
@@ -2,21 +2,39 @@
 https://doh.gov.ph/covid19tracker
 */
 
-var carcases = "carcases.json";
+var carcases = [];
 
-function getCarcasesFromJSON(){
-    fetch('carcases.json').then(function(response) {
+function getCarcasesFromJSON() {
+    fetch('carcases.json').then(function (response) {
         return response.json();
-    }).then(function (obj) {
-        console.log(obj);
-        carcases.push(obj)
-        document.querySelector("#debug").innerHTML = carcases;
-    }).catch(function (error){
+    }).then(function (data) {
+        console.log(JSON.stringify(data));
+        
+        generateCards(data);
+
+    }).catch(function (error) {
         console.error('Something went wrong with retrieving the data.');
     })
 }
 
-var dynamic = document.querySelector('.container_cards');
 
+
+function generateCards(data){
+    var dynamic = document.querySelector('.container_cards');
+    
+    for (var i = 0; i < data[0].Sequence_Key.length; i++) {
+        var fetch = document.querySelector('.container_cards').innerHTML;
+        var tempContainer = data;
+
+        dynamic.innerHTML = 
+
+        `<div class="cards">
+        <div class="card-content">
+            <h2>${data[0].Sequence_Value[i]}</h2>
+            <p>${data[0].Sequence_Key[i]}</p>
+        </div>
+    </div>` + fetch;
+    }
+}
 
 getCarcasesFromJSON();
